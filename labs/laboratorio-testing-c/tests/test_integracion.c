31e44156690e57ad3adc4e38ba121c6530a63f47
Jherson <Jfuentesalegre@frba.utn.edu.ar>
2026-08-12 21:12:19 -0300

    Agrego tests y corrijo bugs en carrito

--- labs/laboratorio-testing-c/tests/test_integracion.c
+++ labs/laboratorio-testing-c/tests/test_integracion.c
@@ -11,19 +11,41 @@
  *  PARTE D — Escribir el test guiado (ver README.md, Parte 8)
  * ═══════════════════════════════════════════════════════════════════════════ */
 
-/* TODO: escribir test_compra_con_descuento() siguiendo la guia del .md */
-
+void test_compra_con_descuento( void){
+    Carrito c;
+    carrito_init(&c);
+    Producto p = {"Pan", 200, 3};
+    Producto l = {"Leche", 350, 2};
+    carrito_agregar(&c, p);
+    carrito_agregar(&c, l);
+    carrito_total(&c);
+    carrito_descuento(carrito_total(&c), 10);
+    ASSERT_IGUAL(1300, carrito_total(&c));
+    ASSERT_IGUAL(1170, carrito_descuento(carrito_total(&c), 10));
+}
 /* ═══════════════════════════════════════════════════════════════════════════
  *  PARTE E — Disenar un test propio (ver README.md, Parte 9)
  * ═══════════════════════════════════════════════════════════════════════════ */
 
-/* TODO: escribir test_agregar_hasta_llenar() */
+void test_agregar_hasta_llenar(void){
+    Carrito c;
+    carrito_init(&c);
+    Producto p = {"Pan", 200, 1};
+    carrito_agregar(&c, p);
+    carrito_agregar(&c, p);
+    carrito_agregar(&c, p);
+    carrito_agregar(&c, p);
+    ASSERT_IGUAL(MAX_ITEMS, carrito_contar(&c));
+    int respuesta = carrito_agregar(&c, p);
+    ASSERT_IGUAL(0, respuesta);
+    ASSERT_IGUAL(MAX_ITEMS, carrito_contar(&c));
+}
 
 int main(void) {
-    printf("=== Tests de integracion ===");
+    printf("=== Tests de integracion ===\n");
     /* Descomentar a medida que agregues las funciones: */
-    /* test_compra_con_descuento();  */
-    /* test_agregar_hasta_llenar();  */
+     test_compra_con_descuento(); 
+     test_agregar_hasta_llenar();  
     RESUMEN();
     return EXIT_CODE();
 }
